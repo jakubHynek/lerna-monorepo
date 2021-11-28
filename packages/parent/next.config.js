0fabff91723f0f36198aa9a685c1840427bde37e
Jakub Hynek <kub.hynek@gmail.com>
2021-11-28 14:55:14 +0100

    fixed next-i18next config

--- packages/parent/next.config.js
+++ packages/parent/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const { i18n } = require('./next-i18next.config');
 
 // next.config.js
 const withTM = require('next-transpile-modules')(['child']); // pass the modules you would like to see transpiled
 
 module.exports = withTM({
+  i18n,
   reactStrictMode: true,
 });
 // module.exports = {
