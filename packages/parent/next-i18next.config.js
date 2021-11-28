4d68da39a3fa552ad63e2083a6c72aab72396c6c
Jakub Hynek <kub.hynek@gmail.com>
2021-11-28 15:34:03 +0100

    fix locale

--- packages/parent/next-i18next.config.js
+++ packages/parent/next-i18next.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
     i18n: {
       defaultLocale: 'cs',
       locales: ['en', 'cs'],
     },
+    localePath: path.resolve('./public/locales'),
   };
