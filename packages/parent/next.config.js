6f1165f8fbc6cc203b2b1a548185b50b3b380fe9
Jakub Hynek <kub.hynek@gmail.com>
2021-11-28 15:19:39 +0100

    test i18n config

--- packages/parent/next.config.js
+++ packages/parent/next.config.js
@@ -5,7 +5,11 @@ const { i18n } = require('./next-i18next.config');
 const withTM = require('next-transpile-modules')(['child']); // pass the modules you would like to see transpiled
 
 module.exports = withTM({
-  i18n,
+  // i18n,
+  i18n: {
+    defaultLocale: 'cs',
+    locales: ['en', 'cs'],
+  },
   reactStrictMode: true,
 });
 // module.exports = {
